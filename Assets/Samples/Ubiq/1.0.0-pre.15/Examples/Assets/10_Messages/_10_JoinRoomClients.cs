41c0f43f60dd3b54694944eae48f15b3e1cdd299
Nelzon Apaza <JoNel2000.FA@gmail.com>
2025-06-30 22:55:34 -0500

    add some coments

--- Assets/Samples/Ubiq/1.0.0-pre.15/Examples/Assets/10_Messages/_10_JoinRoomClients.cs
+++ Assets/Samples/Ubiq/1.0.0-pre.15/Examples/Assets/10_Messages/_10_JoinRoomClients.cs
@@ -8,13 +8,21 @@ using UnityEngine.SceneManagement;
 
 namespace Ubiq.Examples
 {
+    /*
+     Cuando empieza el juego, el código genera un ID único (como un código de sala).
+    Busca todos los "clientes de red" (personas o cosas que se quieren conectar).
+    Los une a la misma sala usando ese ID, para que puedan interactuar entre ellos.
+     */
     public class _10_JoinRoomClients : MonoBehaviour
     {
         private void Start()
         {
+            //Cuando empieza el juego, el código genera un ID único (como un código de sala).
             var guid = Guid.NewGuid();
+            //Busca todos los "clientes de red" (personas o cosas que se quieren conectar).
             foreach (var roomClient in GetComponentsInChildren<RoomClient>())
             {
+                //Los une a la misma sala usando ese ID, para que puedan interactuar entre ellos.
                 roomClient.Join(guid);
             }
         }
--- Assets/Samples/Ubiq/1.0.0-pre.15/Examples/Assets/10_Messages/_10_Messager.cs
+++ Assets/Samples/Ubiq/1.0.0-pre.15/Examples/Assets/10_Messages/_10_Messager.cs
@@ -2,28 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ubiq.Messaging;
+using System;
 
 namespace Ubiq.Examples
 {
     public class _10_Messager : MonoBehaviour
     {
+        //Guarda la "conexión de red" del objeto.
         private NetworkContext context;
 
         private void Start()
         {
+            //"Registra" el objeto en la red usando NetworkScene.Register(this).
             context = NetworkScene.Register(this);
         }
 
         public void SetColor(Color32 color)
         {
+            //Cambia el color del objeto localmente
             GetComponentInChildren<Renderer>().material.color = color;
 
+            //Si está conectado a una red (context.Scene != null), envía el color a los demás usando context.SendJson().
             if (context.Scene != null)
             {
                 context.SendJson<Color32>(color);
             }
         }
 
+        /*
+         Se ejecuta cuando llega un mensaje de otro jugador.
+        Extrae el color del mensaje (message.FromJson<Color32>()) y lo aplica al objeto.
+         */
         public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
         {
             var color = message.FromJson<Color32>();
