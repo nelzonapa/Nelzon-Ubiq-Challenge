4c5820ecfcff28dad5b423266d02a476e82296d9
Nelzon Apaza <JoNel2000.FA@gmail.com>
2025-07-08 07:12:29 -0500

    update and add first node interaction

---
+++ Assets/Mis Assets/Room_Spawn/ScriptsGraph/Nodo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nodo : MonoBehaviour
+{
+    public int idNodo;
+    public string nombreBlogDocumento;
+    public string fragmentoResumen;
+    public string[] palabrasClave;
+    public string[] nombresPropios;
+    public string[] entidades;
+    public string[] concordancias;
+    public int colorGrupo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
