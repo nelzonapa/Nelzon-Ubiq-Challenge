418042657149a6749107fb28e45b5aab8b728e59
equipoEPCC <epcc@epcc.com>
2025-07-08 12:26:52 -0500

    Update scene graph with a message

--- Assets/Mis Assets/Room_Spawn/ScriptsGraph/Nodo.cs
+++ Assets/Mis Assets/Room_Spawn/ScriptsGraph/Nodo.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Nodo : MonoBehaviour
 {
@@ -13,16 +14,47 @@ public class Nodo : MonoBehaviour
     public string[] concordancias;
     public int colorGrupo;
 
+    private MeshRenderer meshRenderer;
+    private Color originalColor;
+    public Color clickedColor = Color.red;
 
-    // Start is called before the first frame update
-    void Start()
+    //Para spawneo:
+
+    [Tooltip("Prefab a instanciar al hacer clic")]
+    public GameObject prefabToSpawn;
+
+    [Tooltip("Punto desde donde se generará el prefab")]
+    public Transform spawnPoint;
+    private GameObject spawnedInstance;
+
+
+    //FUNCIONES
+    private void Awake()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+            originalColor = meshRenderer.material.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnSelectEntered(SelectEnterEventArgs args)
     {
-        
+        if (meshRenderer != null)
+        {
+            meshRenderer.material.color = clickedColor;
+        }
+
+        if (spawnedInstance == null && prefabToSpawn != null && spawnPoint != null)
+        {
+            // Creamos el objeto y lo parentamos para que siga al original
+            spawnedInstance = Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation, transform);
+        }
+    }
+
+    public void OnSelectExited(SelectExitEventArgs args)
+    {
+        if (meshRenderer != null)
+        {
+            meshRenderer.material.color = originalColor;
+        }
     }
 }
